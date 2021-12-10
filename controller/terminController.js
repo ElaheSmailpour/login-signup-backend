2ee1c520311304d110e32c56835cf18dfa8c0d2c
ElaheSmailpour <eli_es87@yahoo.com>
2021-12-10 12:12:35 +0100

    terminbuchen

--- controller/terminController.js
+++ controller/terminController.js
@@ -1,8 +1,8 @@
-
+var cache = require('js-cache');
 const moment = require('moment');
 const behandlung = require('../model/behandlungenModel');
 const terminModel = require('../model/terminModel');
-
+const user = require('../model/userModel');
 exports.getBehandlungen = (req, res, next) => {
     behandlung.find().then((ergebnis) => {
         res.status(200).send(ergebnis)
@@ -17,7 +17,7 @@ exports.getTermin = async (req, res, next) => {
     const avalaibleTimes = []
     for (i = 0; i < 5; i++) {
         const date = {
-            date: moment().add("days",i).format("YYYY-MM-DD"),
+            date: moment().add("days", i).format("YYYY-MM-DD"),
             hours: [10, 11, 12, 13, 14, 15, 16]
         }
         avalaibleTimes.push(date)
@@ -34,4 +34,35 @@ exports.getTermin = async (req, res, next) => {
 
     })
     res.send(avalaibleTimes)
+}
+//getTerminBestätigung
+
+exports.getTerminBestätigung = (req, res, next) => {
+    const phone = req.params.phone;
+    const code = Math.floor(Math.random() * 10000);
+    console.log("code=", code)
+    cache.set(phone, code)
+    res.send(200)
+}
+//buchen
+exports.buchen = async (req, res, next) => {
+    const phone = req.params.phone;
+    const code = req.params.code;
+    const exitCode = cache.get(phone)
+    console.log("exitCode=", exitCode)
+    if (code == exitCode) {
+        let userFind = await user.findOne({ phone: phone })
+        if (!userFind){
+            userFind = await user.create({ name: req.body.name, phone })
+
+    }
+    const createTermin = await terminModel.create({
+        time: req.body.time,
+        date: req.body.date,
+        userId: userFind._id
+    })
+    res.send(createTermin)
+}
+    else
+    res.send(401)
 }
--- routes/terminRouter.js
+++ routes/terminRouter.js
@@ -2,9 +2,10 @@
 const express = require('express')
 const router = express.Router()
 
-const {getBehandlungen,getTermin} = require("../controller/terminController")
+const {getBehandlungen,getTermin,getTerminBestätigung,buchen} = require("../controller/terminController")
 router.get("/",getTermin)
 
+router.route("/verfiyPhone/:phone").get(getTerminBestätigung)
 router.route("/behandlungen").get(getBehandlungen)
-
+router.route("/verfiyPhone/:phone/:code").post(buchen)
 module.exports = router
